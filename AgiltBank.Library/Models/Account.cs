8f3317369130a1c54b1da3006a2519e22cd70be6
Perly Gustafsson <contact@daperley.com>
2017-11-21 19:03:42 +0100

    Added the account model

---
+++ AgiltBank.Library/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace AgiltBank.Library.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
