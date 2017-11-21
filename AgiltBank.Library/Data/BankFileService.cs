05f3826128dd72e5aca5114f138a1a5e4dfc2066
Perly Gustafsson <contact@daperley.com>
2017-11-21 19:39:01 +0100

    Added ParseToAccount to BankFileService.cs

--- AgiltBank.Library/Data/BankFileService.cs
+++ AgiltBank.Library/Data/BankFileService.cs
@@ -1,7 +1,19 @@
-﻿namespace AgiltBank.Library.Data
+﻿using AgiltBank.Library.Models;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace AgiltBank.Library.Data
 {
     public class BankFileService
     {
-
+        private static Account ParseToAccount(IReadOnlyList<string> fields)
+        {
+            return new Account
+            {
+                Id = int.Parse(fields[0]),
+                CustomerId = int.Parse(fields[1]),
+                Balance = decimal.Parse(fields[2], CultureInfo.InvariantCulture)
+            };
+        }
     }
 }
