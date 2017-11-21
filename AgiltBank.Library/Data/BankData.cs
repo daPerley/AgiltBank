45fcf73ee504169991bfdb7eb193fb6a9442af19
Perly Gustafsson <contact@daperley.com>
2017-11-21 19:23:59 +0100

    Added BankData.cs and it's constructor

---
+++ AgiltBank.Library/Data/BankData.cs
@@ -0,0 +1,18 @@
+﻿using AgiltBank.Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AgiltBank.Library.Data
+{
+    public class BankData
+    {
+        public IList<Customer> Customers { get; }
+        public IList<Account> Accounts { get; }
+
+        public BankData(IEnumerable<Customer> customers, IEnumerable<Account> accounts)
+        {
+            Customers = customers.ToList();
+            Accounts = accounts.ToList();
+        }
+    }
+}
