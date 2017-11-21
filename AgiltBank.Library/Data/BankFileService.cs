ab02212f75ed18fe90b4aee1ba54decb2ee34f5c
Perly Gustafsson <contact@daperley.com>
2017-11-21 19:39:25 +0100

    Added ParseToCustomer to BankFileService.cs

--- AgiltBank.Library/Data/BankFileService.cs
+++ AgiltBank.Library/Data/BankFileService.cs
@@ -15,5 +15,21 @@ namespace AgiltBank.Library.Data
                 Balance = decimal.Parse(fields[2], CultureInfo.InvariantCulture)
             };
         }
+
+        private static Customer ParseToCustomer(IReadOnlyList<string> fields)
+        {
+            return new Customer
+            {
+                Id = int.Parse(fields[0]),
+                OrganisationNumber = fields[1],
+                Name = fields[2],
+                StreetAddress = fields[3],
+                City = fields[4],
+                State = fields[5],
+                PostalCode = fields[6],
+                Country = fields[7],
+                PhoneNumber = fields[8]
+            };
+        }
     }
 }
