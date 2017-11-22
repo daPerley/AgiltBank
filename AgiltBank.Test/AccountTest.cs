2d5071a3c55300ef1209807dd532d8b067838a61
Perly Gustafsson <da.perley@yahoo.com>
2017-11-22 11:47:45 +0100

    Feature/transaction (#5)
    * Added deposit logic to the account model
    
    * Added withdrawal logic to account model
    
    * Added the test project
    
    * Added test class for the account transactions
    
    * Added test for Withdrawal of okay amount
    
    * Added test for withdrawal of negative amount
    
    * Added test for withdrawal of an amount higher than account balance
    
    * Added deposit test for okay amount
    
    * Added test for deposit of negative money

---
+++ AgiltBank.Test/AccountTest.cs
@@ -0,0 +1,42 @@
+using AgiltBank.Library.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgiltBank.Test
+{
+    [TestClass]
+    public class AccountTest
+    {
+        private Account _account;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _account = new Account()
+            {
+                Id = 1,
+                CustomerId = 1,
+                Balance = 200
+            };
+        }
+
+        #region Withdrawal
+        [TestMethod]
+        public void CanWithdrawMoneyFromAccount() => Assert.IsTrue(_account.Withdrawal(100));
+
+
+        [TestMethod]
+        public void WithdrawNegativeMoney_ShouldReturnFalse() => Assert.IsFalse(_account.Withdrawal(-100));
+
+        [TestMethod]
+        public void WithdrawMoreMoneyThanAccountBalance_ShouldReturnFalse() => Assert.IsFalse(_account.Withdrawal(10000));
+        #endregion
+
+        #region Deposit
+        [TestMethod]
+        public void CanDepositMoneyToAccount() => Assert.IsTrue(_account.Deposit(100));
+
+        [TestMethod]
+        public void DepositNegativeMoney_ShouldReturnFalse() => Assert.IsFalse(_account.Deposit(-100));
+        #endregion
+    }
+}
