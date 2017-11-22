f427c0727f059bcaf06bddfe667e306f22fc4612
Perly Gustafsson <contact@daperley.com>
2017-11-22 11:38:06 +0100

    Added test class for the account transactions

--- AgiltBank.Test/AccountTest.cs
+++ AgiltBank.Test/AccountTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AgiltBank.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class AccountTest
     {
         [TestMethod]
         public void TestMethod1()
